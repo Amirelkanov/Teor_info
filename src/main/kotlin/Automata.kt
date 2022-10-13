669977a53c9b1a811150fed4bf82a0ecf11a646f
AmEl <amirelkanov0504@gmail.com>
2022-10-14 01:14:25 +0300

    Hotfix

--- src/main/kotlin/Automata.kt
+++ src/main/kotlin/Automata.kt
@@ -1,7 +1,7 @@
 import java.io.File
 
 class Automata(
-    private val numOfStates: Int, private val sizeOfABC: Int,
+    private var numOfStates: Int, private val sizeOfABC: Int,
     private val states: List<List<Int>>, private val startStates: List<Int>,
     private val endStates: List<Int>, private val stringToCheck: MutableList<Int> = emptyList<Int>().toMutableList(),
 ) {
@@ -13,50 +13,45 @@ class Automata(
         check(stringToCheck.all { it in 0 until sizeOfABC }) { throw WrongStringAlphabetException() }
     }
 
-    private val adj = MutableList(numOfStates) { q_i ->
+    private var adj = MutableList(numOfStates) { q_i ->
         states.filter { state -> state.first() == q_i }.map { elem -> elem[2] to elem[1] }.toMutableList()
     }
 
-    private val used = Array(adj.size) { false }
+    private val used = Array(numOfStates) { false }
 
     init {
         unreachableStatesDfs(startStates.first())
-        adj.mapIndexed { i, _ -> if (!used[i]) adj.removeAt(i) }
+        adj = adj.filterIndexed { i, _ -> used[i] }.toMutableList()
+        numOfStates = adj.size
     }
 
-    private fun transitionFun(q: Int, a: Int) = adj[q].first { it.second == a }.first
+    private fun transitionFun(q: Int, a: Int) = adj[q].firstOrNull { it.second == a }?.first
 
     private fun minimizeStates(): MutableList<MutableSet<Int>> {
         val R = mutableListOf(((0 until numOfStates) - endStates.toSet()).toMutableSet(), (endStates.toMutableSet()))
 
         var prevSize = -1
         while (R.size != prevSize) {
-            var hasChangedR = false
-
             for (r_i in R) {
-                for (a in 0 until sizeOfABC) {
-                    if (r_i.map { q -> transitionFun(q, a) }.toMutableSet() !in R) {
-                        prevSize = R.size
-                        R.remove(r_i)
-
-                        r_i.groupBy { q ->
-                            R.indexOfFirst { class_i -> transitionFun(q, a) in class_i }
-                        }.values.map { subclass -> R.add(subclass.toMutableSet()) }
-
-                        hasChangedR = true
-                        break
+                R.remove(r_i)
+                r_i.groupBy { q ->
+                    (0 until sizeOfABC).map { a ->
+                        (transitionFun(q, a) == null) to R.indexOfFirst { class_i ->
+                            (transitionFun(q, a) in class_i && transitionFun(q, a) != null)
+                        }
                     }
-                }
-                if (hasChangedR) break
+                }.values.map { subclass -> R.add(subclass.toMutableSet()) }
+                prevSize = R.size
+                break
             }
         }
-
         return R
     }
 
-    private fun getMinimizedDFA(R: MutableList<MutableSet<Int>>): Triple<Int, MutableList<Int>, MutableList<MutableList<Pair<Int, Int>>>> {
+    private fun getMinimizedDFA(R: MutableList<MutableSet<Int>>): Triple<Int, Pair<Int, MutableList<Int>>, MutableList<MutableList<Pair<Int, Int>>>> {
         val minimizedNumOfStates = R.size
 
+        val minimizedStartState = R.indexOfFirst { class_i -> class_i.any { it == startStates.first() } }
         val minimizedEndStates = mutableListOf<Int>()
         R.forEach { class_i -> if (class_i.first() in endStates) minimizedEndStates.add(R.indexOf(class_i)) }
 
@@ -66,13 +61,15 @@ class Automata(
             }.toSet().toMutableList()
         }
 
-        return Triple(minimizedNumOfStates, minimizedEndStates, minimizedAdj)
+        return Triple(minimizedNumOfStates, minimizedStartState to minimizedEndStates, minimizedAdj)
     }
 
     fun saveMinimizedDFAInFileFormat(fileName: String) { // На самом деле, ее можно было сделать более общей, ну да пусть
-        val (minimizedNumOfStates, minimizedEndStates, minimizedAdj) = getMinimizedDFA(minimizeStates())
+        val (minimizedNumOfStates, minimizedStartAndEndStates, minimizedAdj) = getMinimizedDFA(minimizeStates())
         val strBuilder = StringBuilder()
-        strBuilder.append("$minimizedNumOfStates\n$sizeOfABC\n0\n${minimizedEndStates.joinToString(" ")}\n")
+        strBuilder.append("$minimizedNumOfStates\n$sizeOfABC\n${minimizedStartAndEndStates.first}\n${
+            minimizedStartAndEndStates.second.joinToString(" ")
+        }\n")
         minimizedAdj.mapIndexed { i, neighbors ->
             neighbors.map { pair -> strBuilder.append("$i ${pair.second} ${pair.first}\n") }
         }
